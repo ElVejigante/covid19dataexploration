4a16ccff8b60c2ceb955421d98493d487f4e2744
ElVejigante <ernesto.cancel@gmail.com>
2022-10-04 17:56:41 -0400

    Comments added

---
+++ covid19dataexploration.sql
@@ -0,0 +1,8 @@
+/*
+COVID-19 Data Exploration
+
+Source: https://github.com/owid/covid-19-data/tree/master/public/data
+
+Skills used:
+*/
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+Data Source, Our World in Data: https://github.com/owid/covid-19-data/tree/master/public/data
+
+Skills used:
