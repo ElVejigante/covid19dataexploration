c15739e914e83588fe6082929d39775dab05eaf4
ElVejigante <ernesto.cancel@gmail.com>
2022-10-12 17:36:04 -0400

    total cases vs deaths

--- covid19dataexploration.sql
+++ covid19dataexploration.sql
@@ -18,4 +18,13 @@ FROM covid_19_data
 WHERE continent IS NOT NULL
 ORDER BY 1,2
 
+-- Total Cases vs Total Deaths
+-- Shows likelihood of dying if you contract COVID in your country
+
+SELECT Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS DeathPercentage
+FROM covid_19_data
+WHERE Location like '%states%'
+AND continent IS NOT NULL
+ORDER BY 1,2
+
 
