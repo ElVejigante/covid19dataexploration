9b2167fd2530b5fa445a5c3d81b6c4ad4a7b6e85
ElVejigante <ernesto.cancel@gmail.com>
2022-10-12 17:21:42 -0400

    Skills added

--- covid19dataexploration.sql
+++ covid19dataexploration.sql
@@ -3,6 +3,6 @@ COVID-19 Data Exploration
 
 Source: https://github.com/owid/covid-19-data/tree/master/public/data
 
-Skills used:
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 */
 
