18d879cfe5bf493044f418b5e1c3e84dc5759060
ElVejigante <ernesto.cancel@gmail.com>
2022-10-12 17:23:02 -0400

    First query

--- covid19dataexploration.sql
+++ covid19dataexploration.sql
@@ -6,3 +6,5 @@ Source: https://github.com/owid/covid-19-data/tree/master/public/data
 Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 */
 
+SELECT *
+
