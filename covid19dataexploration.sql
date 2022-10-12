d500bd71de81c89ed9f7e4e7cf7feb343d0e4554
ElVejigante <ernesto.cancel@gmail.com>
2022-10-12 17:32:12 -0400

    Selecting Data

--- covid19dataexploration.sql
+++ covid19dataexploration.sql
@@ -7,4 +7,15 @@ Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, 
 */
 
 SELECT *
+FROM covid_19_data
+WHERE continent IS NOT NULL
+ORDER BY 3,4
+
+-- Selecting Data
+
+SELECT Location, date, total_cases, new_cases, total_deaths, population
+FROM covid_19_data
+WHERE continent IS NOT NULL
+ORDER BY 1,2
+
 
